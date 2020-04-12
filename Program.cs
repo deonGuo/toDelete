ea4ab0e39cc8dc0bcc7863cd93e77b25f32ee1ad
DeonGuo <deon.guo@gmail.com>
2020-04-12 18:21:42 +0100

    commit first change - testing

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace RapfaelSpeech
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("test change commit 1");
         }
     }
 }
